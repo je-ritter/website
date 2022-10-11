43845a1c822fd24ca7453aef01be652a0942f850
je-ritter <jeritter@protonmail.com>
2022-10-11 12:26:14 -0600

    tried deleting several tags and sections

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import jake_bw from './img/JR_BW_Photo.jpeg';
+import React from 'react';
 
 function App() {
   return (
@@ -24,27 +25,15 @@ function App() {
           </div>
       </div>
     </nav>
-   
-    <div className="row">
-      <div className="background">
-        <div className="container">
-          <div className="row">     
-            <div className="front-page card">
-              <div className="row g-0">
-                <div className="front-page col-md-4">
-                  <img src={jake_bw} className="img-fluid rounded-circle"/>
-                </div>
-                <div className="front-page col-md-8">
-                <div className="card-body"> 
-                  <h1 className="card-title">Jake Ritter</h1>
-                  <p className="card-text">
-                    Web Developer based in the Greater Salt Lake area.
-                  </p>    
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+    <div className="front-page col-md-4">
+      <img src={jake_bw} className="img-fluid rounded-circle"/>
+    </div>
+    <div className="front-page col-md-8">
+      <div className="card-body"> 
+        <h1 className="card-title">Jake Ritter</h1>
+        <p className="card-text">
+          Web Developer based in the Greater Salt Lake area.
+        </p>    
       </div>
     </div>
   );
