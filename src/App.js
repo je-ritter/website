4eef42786ca8f5051fb753a46eee9909152bfb61
je-ritter <jeritter@protonmail.com>
2022-10-12 07:07:12 -0600

    in the process still of switching over to GRID and flex

--- src/App.js
+++ src/App.js
@@ -9,22 +9,21 @@ function App() {
   return (
     <div>
       <Nav />
-                <div className="front-page card">
-                  <div className="row g-0">
-                    <div className="front-page col-md-4">
-                      <img src={jake_bw} className="img-fluid rounded-circle"/>
-                    </div>
-                    <div className="front-page col-md-8">
-                    <div className="card-body"> 
-                      <h1 className="card-title">Jake Ritter</h1>
-                      <p className="card-text">
-                        Web Developer based in the Greater Salt Lake area.
-                      </p>    
-                    </div>
-                  </div>
-                </div>
-              </div>
+        <div className="App">
+          <div className="App-card">
+            <div className="App-card--img">
+              <img src={jake_bw} className="img-fluid rounded-circle"/>
             </div>
+            <div className="front-page col-md-8">
+            <div className="card-body"> 
+            <h1 className="card-title">Jake Ritter</h1>
+            <p className="card-text">
+              Web Developer based in the Greater Salt Lake area.
+            </p>    
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
