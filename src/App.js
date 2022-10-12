4aa2bae255eb270de41279298b95beaeef62356d
je-ritter <jeritter@protonmail.com>
2022-10-12 06:34:50 -0600

    trying adding bootstrap...

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
+import React from 'react';
 import './App.css';
 import jake_bw from './img/JR_BW_Photo.jpeg';
-import Nav from './components/navbar/Nav.js';
+import Nav from './components/Nav/Nav.js';
 
 function App() {
   return (
-    <Nav />
-    <div className="row">
-        <div className="background">
+    <div>
+      <Nav />
+        <div className="row">
+          <div className="background">
             <div className="container">
-                <div className="row">    
-                    <div className="front-page card">
-                        <div className="row g-0">
-                            <div className="front-page col-md-4">
-                                <img src={jake_bw}/> className="img-fluid rounded-circle">
-                            </div>
-                            <div className="front-page col-md-8">
-                                <div className="card-body"> 
-                                    <h1 className="card-title">Jake Ritter</h1>
-                                    <p className="card-text">
-                                    Web Developer based in the Greater Salt Lake area.
-                                    </p>    
-                                </div>
-                            </div>
-                        </div>
+              <div className="row">    
+                <div className="front-page card">
+                  <div className="row g-0">
+                    <div className="front-page col-md-4">
+                      <img src={jake_bw} className="img-fluid rounded-circle"/>
                     </div>
+                    <div className="front-page col-md-8">
+                    <div className="card-body"> 
+                      <h1 className="card-title">Jake Ritter</h1>
+                      <p className="card-text">
+                        Web Developer based in the Greater Salt Lake area.
+                      </p>    
+                    </div>
+                  </div>
                 </div>
+              </div>
             </div>
+          </div>
         </div>
-    </div> 
+    </div></div>
   );
 }
 
